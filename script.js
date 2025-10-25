23669cb26bcb1436b7622f3cecc24b0a6e8546cd
QalandarovShokhjahon <shokhjaxonqalandarov@gmail.com>
2025-10-25 14:52:29 +0500

    initial

---
+++ script.js
@@ -0,0 +1,98 @@
+// let userName = 'Shokhjahon';
+// console.log(userName);
+
+// let ism = 'Joe';
+// let familiya = 'Doe';
+// console.log(ism);
+// console.log(familiya);
+
+// let mushuk = 'Max';
+// let egaFamiliya = 'Doe';
+// console.log(mushuk);
+// console.log(egaFamiliya);
+
+// let city = 'Toshkent';
+// let aholisi = '1500000';
+// aholisi = '1550000';
+// console.log('City: ' + city);
+// console.log('Aholisi: ' + aholisi); 
+
+// let son = 10;
+// son = son + 5;
+// console.log(son);
+
+// let a = 8;
+// let b = 3;
+// a = a + b;
+// console.log(a);
+
+// const userName = 'Shokhjahon';
+// userName = 'techWebby';
+// console.log(userName);
+
+// String
+let yosh = 23;
+yosh = 24;
+let ism = 'Joe';
+let tugilganYil = 2001;
+let kasb = 'Frontend dasturchi';
+kasb = 'Full Stack dasturchi';
+let davlat = "O'zbekiston";
+davlat = 'AQSH';
+
+console.log('Mening yoshim: ' + yosh);
+console.log('Mening ismim: ' + ism);
+console.log('Tugilgan yilim: ' + tugilganYil);
+console.log('Mening kasbim: ' + kasb);
+console.log(`Men hozir ${davlat} da yashayabman.`);
+
+let model = 'Malibu';
+let yil = 2020;
+let egalarSoni = 1;
+egalarSoni = 2;
+let joylashuv = 'Toshkent';
+joylashuv = 'Samarqand';
+console.log('Avtomobil modeli: ' + model);
+console.log('Ishlab chiqarilgan yil: ' + yil);
+console.log('Avtomobil egalar soni: ' + egalarSoni);
+console.log('Avtomobil hozir: ' + joylashuv);
+
+// Number
+let olma = 3.5;
+let banan = 2.0;
+let uzum = 5.0;
+let jami = olma + banan + uzum;
+console.log(jami);
+
+let length = 15;
+let width = 7.5;
+let height = 20;
+console.log(length + width + height);
+
+// Boolean
+let quyosh = true;
+let yomgir = false;
+let qor = false;
+console.log('Quyosh nuri mavjudmi: ' + quyosh);
+console.log('Yomgir yogadimi: ' + yomgir);
+console.log('Qor yogadimi: ' + qor);
+
+// Null
+let userName = '';
+let familiya = null;
+console.log(ism);
+console.log(familiya);
+
+// Object
+let car = {
+    model: 'cobalt',
+    color: 'oq',
+    isNew: true
+}
+console.log('Mashinaning rangi: ' + car.color);
+console.log('Mashina yangi: ' + car.isNew);
+
+// Array
+let mevalar = ['olma', 'banan', 'uzum']
+console.log(mevalar[1]);
+
